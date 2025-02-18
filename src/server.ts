ef5536fd0f7230acb98398c63396cf35e0f01d11
Felipe Carvalho <felipecarvalho180@gmail.com>
2025-02-17 21:54:56 -0300

    day 1

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { fastifyCors } from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventSchema } from './routes/subscribe-to-event-route'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Conect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventSchema)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running! 🚀')
+})
